7215e1448fb38f12533b790e1389ca7a52e3d0e7
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-14 23:33:53 -0300

    Add some Emojies

--- SendSlackMessage/Entities/Emoji.cs
+++ SendSlackMessage/Entities/Emoji.cs
@@ -5,21 +5,67 @@
         //Please keep the alphabetical order
 
         /* A-E */
+        public const string AlarmClock = ":alarm_clock:";
+        public const string Alien = ":alien:";
         public const string Ambulance = ":ambulance:";
+        public const string ArrowBackward = ":arrow_backward:";
+        public const string ArrowDoubleDown = ":arrow_double_down:";
+        public const string ArrowDoubleUp = ":arrow_double_up:";
+        public const string ArrowDown = ":arrow_down:";
+        public const string ArrowDownSmall = ":arrow_down_small:";
+        public const string ArrowForward = ":arrow_forward:";
+        public const string ArrowHeadingDown = ":arrow_heading_down:";
+        public const string ArrowHeadingUp = ":arrow_heading_up:";
+        public const string ArrowLeft = ":arrow_left:";
+        public const string ArrowLowerLeft = ":arrow_lower_left:";
+        public const string ArrowLowerRight = ":arrow_lower_right:";
+        public const string ArrowRight = ":arrow_right:";
+        public const string ArrowRightHook = ":arrow_right_hook:";
+        public const string ArrowsClockwise = ":arrows_clockwise:";
+        public const string ArrowsCounterclockwise = ":arrows_counterclockwise:";
+        public const string ArrowUp = ":arrow_up:";
+        public const string ArrowUpDown = ":arrow_up_down:";
+        public const string ArrowUpperLeft = ":arrow_upper_left:";
+        public const string ArrowUpperRight = ":arrow_upper_right:";
+        public const string ArrowUpSmall = ":arrow_up_small:";
+        public const string BallotBoxWithCheck = ":ballot_box_with_check:";
+        public const string Bell = ":bell:";
+        public const string BlackCircle = ":black_circle:";
+        public const string Bomb = ":bomb:";
+        public const string BustInSilhouette = ":bust_in_silhouette:";
+        public const string BustsInSilhouette = ":busts_in_silhouette:";
+        public const string Calendar = ":calendar:";
         public const string Check_Green_White = ":white_check_mark:";
         public const string Check_Green_BorderBlack = ":heavy_check_mark:";
         public const string Coffee = ":coffee:";
         public const string Error_X = ":x:";
+        public const string Euro = ":euro:";
         public const string Eye = ":eye:";
 
         /* F - J*/
+        public const string Five = ":five:";
+        public const string FloppyDisk = ":floppy_disk:";
+        public const string Four = ":four:";
 
         /* K - O */
+        public const string LargeBlueCircle = ":large_blue_circle:";
+        public const string Link = ":link:";
+        public const string One = ":one:";
+        public const string OneHundred = ":100:";
+        public const string OneTwoThreeFour = ":1234:";
+        public const string OpenFileFolder = ":open_file_folder:";
+        public const string Paperclip = ":paperclip:";
 
         /* P - T */
         public const string Troll_Face = ":trollface:";
+        public const string Sound = ":sound:";
+        public const string Telephone = ":telephone:";
+        public const string TelephoneReceiver = ":telephone_receiver:";
 
         /* U - Z */
         public const string Warning = ":warning:";
+        public const string WhiteCheckMark = ":white_check_mark:";
+        public const string WhiteCircle = ":white_circle:";
+        public const string Zero = ":zero:";
     }
 }
