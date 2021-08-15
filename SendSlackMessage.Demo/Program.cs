ddb4e6d5ae4bc4a7036839f3b5d1f20e34eef4b0
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-14 22:06:41 -0300

    Add Emoji options

--- SendSlackMessage.Demo/Program.cs
+++ SendSlackMessage.Demo/Program.cs
@@ -4,26 +4,31 @@ using System.Configuration;
 
 namespace SendSlackMessage.Demo
 {
-    class Program
+    internal static class Program
     {
         private static readonly string _webHookUrl = ConfigurationManager.AppSettings.Get("WebHookUrl").ToString(); //PUT YOUR WEB HOOK URL IN App.Config[WebHookUrl] 
         private static readonly string _username = ConfigurationManager.AppSettings.Get("UserName").ToString(); //PUT YOUR USER IN App.Config[UserName]
-        private static readonly string _iconEmoji = ConfigurationManager.AppSettings.Get("IconEmoji").ToString(); //PUT YOUR USER IN App.Config[IconEmoji] - Optional
         private static readonly string _iconUrl = ConfigurationManager.AppSettings.Get("IconUrl").ToString(); //PUT YOUR USER IN App.Config[IconUrl] - Optional
         static void Main(string[] args)
         {
             Console.WriteLine("Initializing demo...");
+            //Get or Create your webHookUrl here: https://my.slack.com/services/new/incoming-webhook/
+            try
+            {
+                var client = new SsmClient(_webHookUrl);
+                Console.WriteLine("Write the text and press Enter to send to Slack:");
+                string text = Console.ReadLine();
 
-            //Get your webHookUrl here: https://my.slack.com/services/new/incoming-webhook/
-            var client = new SsmClient(_webHookUrl);
-            Console.WriteLine("Write the text and press Enter to send to Slack:");
-            string text = Console.ReadLine();
+                var message = new Message(_username, Emoji.Coffee, _iconUrl, text);
+                Console.WriteLine("Sending message to Slack...");
 
-            var message = new Message(_username, _iconEmoji, _iconUrl, text);
-            Console.WriteLine("Sending message to Slack...");
-
-            var response = client.Send(message);
-            Console.WriteLine(response.Result.ToString());
+                var response = client.Send(message);
+                Console.WriteLine(response.Result.ToString());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An error ocurred: {ex}");
+            }
             Console.ReadLine();
         }
     }
---
+++ SendSlackMessage/Entities/Emoji.cs
@@ -0,0 +1,25 @@
+﻿namespace SendSlackMessage.Entities
+{
+    public static class Emoji
+    {
+        //Please keep the alphabetical order
+
+        /* A-E */
+        public const string Ambulance = ":ambulance:";
+        public const string Check_Green_White = ":white_check_mark:";
+        public const string Check_Green_BorderBlack = ":heavy_check_mark:";
+        public const string Coffee = ":coffee:";
+        public const string Error_X = ":x:";
+        public const string Eye = ":eye:";
+
+        /* F - J*/
+
+        /* K - O */
+
+        /* P - T */
+        public const string Troll_Face = ":trollface:";
+
+        /* U - Z */
+        public const string Warning = ":warning:";
+    }
+}
