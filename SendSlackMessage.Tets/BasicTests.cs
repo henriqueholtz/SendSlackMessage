a58bddca83a9819a27652be4368fc2e4e4914173
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-16 23:54:23 -0300

    Create SendSlackMessage.Tests with NUnit

---
+++ SendSlackMessage.Tets/BasicTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SendSlackMessage.Tets
+{
+    public class BasicTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
