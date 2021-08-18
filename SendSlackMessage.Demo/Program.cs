54578a7e40312bc31e8eeb4d39054e941af1a5d0
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-17 23:15:44 -0300

    Creating dynamic mode to demo project

--- SendSlackMessage.Demo/Program.cs
+++ SendSlackMessage.Demo/Program.cs
@@ -7,11 +7,12 @@ namespace SendSlackMessage.Demo
     internal static class Program
     {
         private static readonly string _webHookUrl = ConfigurationManager.AppSettings.Get("WebHookUrl").ToString(); //PUT YOUR WEB HOOK URL IN App.Config[WebHookUrl] 
-        private static readonly string _username = ConfigurationManager.AppSettings.Get("UserName").ToString(); //PUT YOUR USER IN App.Config[UserName]
+        private static string _username = ConfigurationManager.AppSettings.Get("UserName").ToString(); //PUT YOUR USER IN App.Config[UserName]
         private static readonly string _iconUrl = ConfigurationManager.AppSettings.Get("IconUrl").ToString(); //PUT YOUR USER IN App.Config[IconUrl] - Optional
-        private static readonly string _channelOverride = ConfigurationManager.AppSettings.Get("Channel").ToString(); //PUT YOUR USER IN App.Config[Channel] - Optional (to override channel of web-hook)
+        private static string _channelOverride = ConfigurationManager.AppSettings.Get("Channel").ToString(); //PUT YOUR USER IN App.Config[Channel] - Optional (to override channel of web-hook)
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Demo of ** SendSlackMessage **");
             Console.WriteLine("Initializing demo...");
             //Get or Create your webHookUrl here: https://my.slack.com/services/new/incoming-webhook/
             //Some instructions here: https://github.com/henriqueholtz/SendSlackMessage/blob/master/README.md
@@ -19,36 +20,76 @@ namespace SendSlackMessage.Demo
             {
                 var client = new SsmClient(_webHookUrl);
 
+                #region channel
+                if (String.IsNullOrWhiteSpace(_channelOverride))
+                {
+                    Console.WriteLine("Couldn't possible get the channel from your config. Write (or leave blank to no override) and press Enter.");
+                    _channelOverride = Console.ReadLine();
+
+                }
+                #endregion
+
+                #region username
+                Console.WriteLine();
+                if (String.IsNullOrWhiteSpace(_username))
+                {
+                    Console.WriteLine("Couldn't possible get the username from your config. Write (or leave blank to no override) and press Enter.");
+                    _username = Console.ReadLine();
+                }
+                #endregion
+
                 for (var i = 0; i < 100; i++)
                 {
-                    Console.WriteLine("----------------------------------------------------------------------------------------------");
-                    string channel = _channelOverride;
-                    if (String.IsNullOrWhiteSpace(channel))
-                    {
-                        Console.WriteLine("Couldn't possible get the channel from your config. Write (or leave blank to no override) and press Enter.");
-                        channel = Console.ReadLine();
-                    }
                     Console.WriteLine();
-                    string username = _username;
-                    if (String.IsNullOrWhiteSpace(username))
+                    Console.WriteLine("----------------------------------------------------------------------------------------------");
+                    Console.Write("Press key A to dynamic mode, or another to entry in the menu with some options: ");
+                    var option = Console.ReadKey();
+                    Console.WriteLine();
+                    Message message = null;
+                    if (option.Key == ConsoleKey.A)
+                        message = DynamicMode();
+                    else
                     {
-                        Console.WriteLine("Couldn't possible get the username from your config. Write (or leave blank to no override) and press Enter.");
-                        username = Console.ReadLine();
+                        Console.WriteLine("Loading menu of options....");
+                        Console.WriteLine();
+                        MenuOptions();
                     }
-                    Console.WriteLine("Write the text and press Enter to send to Slack:");
-                    string text = Console.ReadLine();
-                    var message = new Message(channel, username, Emoji.Coffee, _iconUrl, text, true);
-                    Console.WriteLine("Sending message to Slack...");
 
-                    var response = client.Send(message);
-                    Console.WriteLine(response.Result.ToString());
+                    if (message != null)
+                    {
+                        Console.WriteLine("Sending message to Slack...");
+                        var response = client.Send(message);
+                        Console.WriteLine(response.Result.ToString());
+                    }
                 }
             }
             catch(Exception ex)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"An error ocurred: {ex}");
+                Console.ResetColor();
             }
             Console.ReadLine();
+        }
+
+        static Message DynamicMode()
+        {
+            Console.WriteLine("Entry in dynamic mode....");
+            Console.WriteLine();
+
+            Console.WriteLine("Write the text and press Enter to send to Slack:");
+            string text = Console.ReadLine();
+            return new Message(_channelOverride, _username, Emoji.Coffee, _iconUrl, text, true);
+        }
+
+        static void MenuOptions()
+        {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("---            Menu             ---");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Option in construction, select dynamic mode please.");
+            //Console.WriteLine("1- ");
+            //Console.WriteLine("2- ");
         }
     }
 }
