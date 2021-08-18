7eb94d162ba544d76fe19558248c6418ab3b5f4a
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-17 23:46:13 -0300

    Add some constructors and methods (AddAttachment for example)

--- SendSlackMessage/Entities/Message.cs
+++ SendSlackMessage/Entities/Message.cs
@@ -22,20 +22,33 @@ namespace SendSlackMessage.Entities
 
         [JsonPropertyName("text")]
         public string Text { get; private set; }
+
         [JsonPropertyName("mrkdwn")]
-        public bool MarkDown { get; private set; }
+        public bool MarkDown { get; private set; } = true;
         private string _responseType;
 
         [JsonPropertyName("response_type")]
         private string ResponseType { get => string.IsNullOrWhiteSpace(_responseType) ? "ephemeral" : _responseType; set => _responseType = value; }
 
-
         [JsonPropertyName("attachments")]
-        public List<Attachment> Attachments { get; set; }
+        public List<Attachment> Attachments { get; set; } = new List<Attachment>();
 
-        public Message(string channel, string username, string iconEmoji, string iconUrl, string text, bool markdown = true)
+
+        /* Constructors */
+        public Message(string username, string text) 
         {
-            Channel = channel;
+            Username = username;
+            Text = text;
+        }
+        public Message(string channelOverride, string username, string text)
+        {
+            Username = username;
+            Text = text;
+            Channel = channelOverride;
+        }
+        public Message(string channelOverride, string username, string iconEmoji, string iconUrl, string text, bool markdown = true)
+        {
+            Channel = channelOverride;
             Username = username;
             IconEmoji = iconEmoji;
             IconUrl = iconUrl;
@@ -43,29 +56,43 @@ namespace SendSlackMessage.Entities
             MarkDown = markdown;
         }
 
+
+        /* Methods */
+        public Message SetText(string text)
+        {
+            Text = text;
+            return this;
+        }
         public Message SetChannel(string channelOverride = "")
         {
             Channel = channelOverride;
             return this;
         }
-
         public Message SetUserWithEmoji(string username, string iconEmoji)
         {
             Username = username;
             IconEmoji = iconEmoji;
             return this;
         }
-
         public Message SetUserWithIconUrl(string username, string iconUrl)
         {
             Username = username;
             IconUrl = iconUrl;
             return this;
         }
-
         public Message SetResponseType(EnumResponseType responseType)
         {
             ResponseType = responseType.ToString();
+            return this;
+        }
+        public Message AddAttachment(Attachment attachment)
+        {
+            Attachments.Add(attachment);
+            return this;
+        }
+        public Message SetMarkdwon(bool markdown)
+        {
+            MarkDown = markdown;
             return this;
         }
     }
