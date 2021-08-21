45ae59200fffe9a05f282ee33be6efaff857c765
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-20 22:55:59 -0300

    create static class 'Options' to reuse soon with Tests

---
+++ SendSlackMessage.Demo/Options.cs
@@ -0,0 +1,39 @@
+﻿using SendSlackMessage.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendSlackMessage.Demo
+{
+    public static class Options
+    {
+        public static Message GetOptionByCode(int code)
+        {
+            switch (code)
+            {
+                case 1:
+                    return new Message("override after", "SendSlackMessage - 1", Emoji.Coffee, "", "This is a first option predefined.");
+                case 2:
+                    return new Message("override after", "You are using SendSlackMessages by Henrique Holtz.", new List<Attachment>
+                    {
+                        new Attachment("This is the first Line (Attachment)"),
+                        new Attachment("This is the second Line (Attachment)", "good", new List<Field>()
+                        )
+                    });
+                case 3:
+                    return new Message("override after", "You are using SendSlackMessages by Henrique Holtz.", new List<Attachment>
+                    {
+                        new Attachment("This is the first Line (Attachment) - without field"),
+                        new Attachment("This is the second Line (Attachment) with field", "good", new List<Field>()
+                        {
+                            new Field(Emoji.HeavyCheckMark + " Success", "Value (Field)")
+                        })
+                    });
+                default:
+                    return new Message("override after", "SendSlackMessage - Default", Emoji.Bomb, "", "This is a default option predefined.");
+            }
+        }
+    }
+}
--- SendSlackMessage.Demo/Program.cs
+++ SendSlackMessage.Demo/Program.cs
@@ -94,8 +94,8 @@ namespace SendSlackMessage.Demo
             Console.WriteLine("---            Menu             ---");
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("1- Simple option");
-            Console.WriteLine("1- Simple option with Attachments");
-            Console.WriteLine("1- Simple option with Attachments and Fields");
+            Console.WriteLine("2- Simple option with Attachments");
+            Console.WriteLine("3- Simple option with Attachments and Fields");
 
             string option = Console.ReadLine();
             int opt = 0;
@@ -110,29 +110,7 @@ namespace SendSlackMessage.Demo
         static Message Option(int option)
         {
             Console.WriteLine($"Selected: {option}");
-            switch (option)
-            {
-                case 1:
-                    return new Message("override after", "SendSlackMessage - 1", Emoji.Coffee, "", "This is a first option predefined.");
-                case 2:
-                    return new Message("override after", "You are using SendSlackMessages by Henrique Holtz.", new List<Attachment>
-                    {
-                        new Attachment("This is the first Line (Attachment)"),
-                        new Attachment("This is the second Line (Attachment)", "good", new List<Field>()
-                        )
-                    });
-                case 3:
-                    return new Message("override after", "You are using SendSlackMessages by Henrique Holtz.", new List<Attachment>
-                    {
-                        new Attachment("This is the first Line (Attachment) - without field"),
-                        new Attachment("This is the second Line (Attachment) with field", "good", new List<Field>()
-                        {
-                            new Field(Emoji.HeavyCheckMark + " Success", "Value (Field)")
-                        })
-                    });
-                default:
-                    return new Message("override after", "SendSlackMessage - Default", Emoji.Bomb, "", "This is a default option predefined.");
-            }
+            return Options.GetOptionByCode(option);
         }
     }
 }
