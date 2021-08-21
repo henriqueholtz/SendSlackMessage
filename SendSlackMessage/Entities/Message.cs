131bcf222c143dbfce5f5392d90641aa2c365a7a
henriqueholtz <henrique_holtz@hotmail.com>
2021-08-20 22:33:35 -0300

    Removing SsmHelper (because FluentValidation has removed)

--- SendSlackMessage/Entities/Message.cs
+++ SendSlackMessage/Entities/Message.cs
@@ -1,5 +1,4 @@
 ﻿using SendSlackMessage.Enums;
-using SendSlackMessage.Helpers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
--- SendSlackMessage/Exceptions/InvalidUrlException.cs
+++ SendSlackMessage/Exceptions/InvalidUrlException.cs
@@ -4,7 +4,7 @@ namespace SendSlackMessage.Exceptions
 {
     public class InvalidUrlException : Exception
     {
-        public InvalidUrlException(string url,string message = "This Url don't is valid. ") : base(message + url)
+        public InvalidUrlException(string url, string message = "This Url don't is valid. ") : base(message + url)
         {
         }
     }
